e000a515e165ac6a3dc63e0a93aebb84ea3f43f0
Vince Ruiz <vmr846@mail.usask.ca>
2023-12-29 23:26:03 -0600

    Added tailwind support; Changes to Home.js

--- frontend/leger/src/Components/Home/Home.js
+++ frontend/leger/src/Components/Home/Home.js
@@ -5,6 +5,17 @@ function Home() {
   return (
     <div>
       <TopNav />
+      <div className='summary1'>
+        <h2>A glimpse into my journey as a computer engineer</h2>
+      </div>
+      <div className='workSummary'>
+        <h2>Computer Engineer by day</h2>
+        <p></p>
+      </div>
+      <div className='aboutSummary'>
+        <h2>Human being by night</h2>
+      </div>
+
     </div>
   )
 }
