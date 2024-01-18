e5a3910a1e18d4908575963e6bdb05006ab25581
Vince Ruiz <vmr846@mail.usask.ca>
2024-01-18 13:14:06 -0600

    Added GET and POST endpoints in server.js || Changes to dockerfile and docker-compose.yml

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
-const bodyParser= require('body-parser')
+const bodyParser= require('body-parser');
+const { query } = require('express');
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,13 @@ app.use(bodyParser.json());
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
+const db_config = {
+    host: 'db',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+};
+
 function apiKeyMiddleware(req, res, next) {
     const apiKey = req.header('X-API-KEY');
     if (!apiKey || apiKey !== process.env.MY_API_KEY) {
@@ -22,4 +30,37 @@ function apiKeyMiddleware(req, res, next) {
     next();
 }
 
-let connection;
+const pool = mysql.createPool(db_config);
+
+// ============= ENDPOINTS =============
+app.route('/project')
+    .get(apiKeyMiddleware, (req, res) => { // get all projects
+        const query = `SELECT * FROM projects`;
+        pool.query(query, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({message: "server error"});
+            }
+            if (result.length < 1) {
+                return res.status(204).send({ message: 'No posts to display' });
+            }
+            res.status(200).json(result);
+        })
+    })
+    .post(apiKeyMiddleware, (req,res) => { // create new project
+        const query = `INSERT INTO projects (title, working_name, preview_description, full_description, img_url, github) VALUES (?, ?, ?, ?, ?, ?)`;
+        pool.query(query, (err, result) => {
+            if (err) throw err;
+            res.status(200).send({ message: 'Project added successfully' });
+        })      
+    })
+    .put(apiKeyMiddleware, (req,res) => { // update project
+
+    })
+    .delete(apiKeyMiddleware, (req,res) => { // delete project
+
+    })
+
+
+app.listen(PORT, HOST);
+console.log('Server is up and running...');
